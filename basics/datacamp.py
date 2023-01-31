906e858c4184991c901f5eeb2cc5698724330270
NiJoshua <joshkobeng@gmail.com>
2023-01-31 15:17:59 +0000

    updates

---
+++ basics/datacamp.py
@@ -0,0 +1,9 @@
+import streamlit as st #st is used to add anything to a web app
+st.write("Hello, let's learn how to build a streamlit app")
+st.title("This is the title app") # allows us to add title
+st.header("This is the markdown") # used to set header of a section
+st.markdown("This is the header") # used to set markdown
+st.subheader("This is the subheader") #used to set subheader
+st.caption("This is the caption") #used to write caption
+st.code("x = 2023") # used to set a code
+st.latex(r'''a+a r^1+a r^2+a r^3 ''') #used to display mathematical expressions formatted as LaTex
