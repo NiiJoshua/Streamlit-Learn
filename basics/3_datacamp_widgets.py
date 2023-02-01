72a5484b23f27af8e290acab2c7b292dc9982a9c
NiJoshua <joshkobeng@gmail.com>
2023-02-01 13:53:21 +0000

    update

--- basics/3_datacamp_widgets.py
+++ basics/3_datacamp_widgets.py
@@ -3,3 +3,5 @@ import streamlit as st
 st.checkbox("yes")
 st.button('Click')
 st.radio('Pick your gender',['Male','Female'])
+st.selectbox('Pick your gender',['Male','Female'])
+st.multiselect('Choose a planet',['Jupiter','Mars','Neptune'])
