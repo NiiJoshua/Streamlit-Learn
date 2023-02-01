f6b9f8099b46d5130026e91e3ea096b03a77954c
NiJoshua <joshkobeng@gmail.com>
2023-02-01 15:55:15 +0000

    update

--- basics/6_datacamp_error.py
+++ basics/6_datacamp_error.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+# container
+container = st.container()
+container.write('This is written inside the container')
+st.write('This is written outside the container')
+
 st.success("Congratulations, you did it !")
 st.error("Error")
 st.warning("Warning")
