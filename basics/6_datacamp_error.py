0ccde6e870dec5986a49e558093fd375e3e65bc1
NiJoshua <joshkobeng@gmail.com>
2023-02-01 15:48:00 +0000

    update

---
+++ basics/6_datacamp_error.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.success("Congratulations, you did it !")
+st.error("Error")
+st.warning("Warning")
+st.info("It is easier to build a streamlit app")
+st.exception(RuntimeError("RuntimeError exception"))
