8bda2824e5ce98178d1ce47b148f7bb3de282f9b
NiJoshua <joshkobeng@gmail.com>
2023-02-02 12:39:07 +0000

    update

--- basics/7_datacamp_graphs.py
+++ basics/7_datacamp_graphs.py
@@ -6,6 +6,7 @@ import numpy as np
 st.title('Collection of graphs')
 st.subheader('Bar graph')
 
+# Bar Graph
 rand = np.random.normal(1,2, size=20)
 fig, ax = plt.subplots()
 ax.hist(rand, bins=15)
