7740274264c91bbf620b0caefef1df33684f3c99
NiJoshua <joshkobeng@gmail.com>
2023-02-02 14:14:12 +0000

    update

--- basics/8_datacamp_ml.py
+++ basics/8_datacamp_ml.py
@@ -4,3 +4,10 @@ import pandas as pd
 import numpy as np
 import pickle #to load a saved model
 import base64 #to open .gif files in streamlit
+
+@st.cache(suppress_st_warning=True) # cache makes performant easier
+def get_fvalue(val):
+    feature_dict = {"No":1, "Yes":2}
+    for key, value in feature_dict.items():
+        if val == key:
+            return value
