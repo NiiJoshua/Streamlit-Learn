307e3719c305e6860247466e3d29a72faf030cbb
NiJoshua <joshkobeng@gmail.com>
2023-02-02 12:42:28 +0000

    update

--- basics/7_datacamp_graphs.py
+++ basics/7_datacamp_graphs.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 st.title('Collection of graphs')
 st.subheader('Bar graph')
@@ -11,3 +12,9 @@ rand = np.random.normal(1,2, size=20)
 fig, ax = plt.subplots()
 ax.hist(rand, bins=15)
 st.pyplot(fig)
+
+# Libe Graph
+st.subheader('Line graph')
+
+df = pd.DataFrame(np.random(10,2), columns=["x","y"])
+st.line_chart(df)
